cd783806c7b18e2126be8e877ed33ff49b227941
Nathan Nicolle <nathan-nicolle@orange.fr>
2020-11-23 18:23:45 +0100

    modification global de la page

--- tp2/ex7.php
+++ tp2/ex7.php
@@ -9,28 +9,34 @@
         <?php 
             include("functions.php");
         ?>
-        <form method="post">
-            <label for="chaine">Entrez votre texte :</label>
-            <textarea name="chaine" rows="2"></textarea>
-            <select name="select">
-                <option value="nbword">Nombre de mots</option>
-                <option value="nbcarac">Nombre de caractères</option>
-            </select>
-            <label for="carac">Caractère ?</label>
-            <input type="text" name="carac">
-            <input type="submit" value="compter" id="compter">
-        </form>
-        <h1>Résultat</h1>
-        <p><?php  
-            switch ($_POST['select']) {
-                case 'nbword' :
-                    wordCount();
-                break;
-                case 'nbcarac' :
-                    charCount();
-                break;
-            }
-        ?>
-        </p>
+        <div class="content">
+            <form method="post">
+                <label for="chaine">Entrez votre texte :</label>
+                <textarea name="chaine" rows="2"></textarea>
+                <select name="select">
+                    <option value="nbword">Nombre de mots</option>
+                    <option value="nbcarac">Nombre de caractères</option>
+                </select>
+                <label for="carac">Caractère à compter :</label>
+                <input type="text" name="carac">
+                <input type="submit" value="compter" id="compter">
+            </form>
+            <div class="result">
+                <h1>Résultat</h1>
+                <p><?php  
+                    switch ($_POST['select']) {
+                        case 'nbword' :
+                            echo "Nombre de mots :<br>";
+                            wordCount();
+                        break;
+                        case 'nbcarac' :
+                            echo "Nombre de caractères :<br>";
+                            charCount();
+                        break;
+                    }
+                ?>
+                </p>
+            </div>
+        </div>
     </body>
 </html>
